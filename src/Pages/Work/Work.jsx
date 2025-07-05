892964a1492bf5c33616eecedabe40b8fc660bdc
Weeny_Coder <ebrahimislam2003@gmail.com>
2025-07-06 01:37:50 +0600

    done

--- src/Pages/Work/Work.jsx
+++ src/Pages/Work/Work.jsx
@@ -3,14 +3,17 @@ import ImgMediaCard from '../../Component/Shared/ImgMediaCard';
 import useProject from '../../Data/useProject';
 
 const Work = () => {
-    const {projects} = useProject();
+    const { projects } = useProject();
     return (
-        <div className="flex flex-wrap justify-center">
-            {
-                projects?.map(project => (
-                    <ImgMediaCard key={project.id} project={project} />
-                ))
-            }
+        <div className="">
+            <h1 className='text-center text-2xl'>NB: For Better experience turn your pc theme into Light</h1>
+            <div className="flex flex-wrap justify-center">
+                {
+                    projects?.map(project => (
+                        <ImgMediaCard key={project.id} project={project} />
+                    ))
+                }
+            </div>
         </div>
     );
 };
