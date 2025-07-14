7803eae5d2ecd9993286a490dbc426d3003e2463
EbrahimIslamShuvo <ebrahimislam2003@gmail.com>
2025-07-14 18:41:56 +0600

    fix not found problem

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from './Component/NavBar';
 import Footer from './Component/Footer';
+import ScrollToTop from './Component/ScrollToTop';
 
 const Root = () => {
     return (
         <div className="">
+            <ScrollToTop />
             <div className="">
-                <NavBar />
+                <div className="">
+                    <NavBar />
+                </div>
+
+                <main className="w-6/12 mx-auto -mt-130 ml-145 relative z-50">
+                    <Outlet />
+                </main>
+
+                <Footer />
             </div>
-
-            <main className="w-6/12 mx-auto -mt-130 ml-145 relative z-50">
-                <Outlet />
-            </main>
-
-            <Footer />
         </div>
     );
 };
